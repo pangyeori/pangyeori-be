2fd9861aa838e9c54ee86131bcb8eb4497a75ae4
DEOKJAE KWON <juintin@kakao.com>
2026-08-03 16:49:20 +0900

    build: plugin.jpa 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "2.3.21"
     id("org.springframework.boot") version "4.1.0"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "2.3.21"
 }
 
 group = "com.debate"
