5e75672e8c3a5259522849f4c772c48e44762ba5
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2026-08-10 00:10:10 +0900

    feat: 로그인 관련 exception 추가

---
+++ src/main/kotlin/com/debate/pangyeori/user/exception/InvalidCredentialsException.kt
@@ -0,0 +1,6 @@
+package com.debate.pangyeori.user.exception
+
+import com.debate.pangyeori.common.exception.BusinessException
+import com.debate.pangyeori.common.exception.ErrorCode
+
+class InvalidCredentialsException : BusinessException(ErrorCode.INVALID_CREDENTIALS)
---
+++ src/main/kotlin/com/debate/pangyeori/user/exception/InvalidTokenException.kt
@@ -0,0 +1,6 @@
+package com.debate.pangyeori.user.exception
+
+import com.debate.pangyeori.common.exception.BusinessException
+import com.debate.pangyeori.common.exception.ErrorCode
+
+class InvalidTokenException : BusinessException(ErrorCode.INVALID_TOKEN)
