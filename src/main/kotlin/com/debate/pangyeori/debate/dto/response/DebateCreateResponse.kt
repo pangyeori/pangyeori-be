7bf073749c3f1c63ddadcb3110000d8b261fb9e1
DEOKJAE KWON <juintin@kakao.com>
2026-09-05 22:18:12 +0900

    refactor: DebateMemberResponse를 별도 파일로 분리

--- src/main/kotlin/com/debate/pangyeori/debate/dto/response/DebateCreateResponse.kt
+++ src/main/kotlin/com/debate/pangyeori/debate/dto/response/DebateCreateResponse.kt
@@ -4,8 +4,6 @@ import com.debate.pangyeori.debate.domain.Debate
 import com.debate.pangyeori.debate.domain.DebateUser
 import com.debate.pangyeori.debate.domain.enums.DebatePosition
 import com.debate.pangyeori.debate.domain.enums.DebateStatus
-import com.debate.pangyeori.debate.domain.enums.DebateUserRole
-import com.debate.pangyeori.debate.domain.enums.DebateUserStatus
 
 data class DebateCreateResponse(
     val id: String,
@@ -38,26 +36,6 @@ data class DebateCreateResponse(
                     debateUser = hostMember,
                 ),
             ),
-        )
-    }
-}
-
-data class DebateMemberResponse(
-    val userId: String,
-    val nickname: String,
-    val role: DebateUserRole,
-    val position: DebatePosition,
-    val status: DebateUserStatus,
-) {
-    companion object {
-        fun from(
-            debateUser: DebateUser,
-        ) = DebateMemberResponse(
-            userId = debateUser.user.id!!,
-            nickname = debateUser.user.nickname,
-            role = debateUser.role,
-            position = debateUser.position,
-            status = debateUser.status,
         )
     }
 }
