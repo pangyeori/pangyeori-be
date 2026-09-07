cba930ef6e3ac1f5c37c1efef795bad680b0e49d
DEOKJAE KWON <juintin@kakao.com>
2026-09-07 16:40:09 +0900

    test: RestDocsMvcTest에서 롤백 직전 flush 강제

--- src/test/kotlin/com/debate/pangyeori/support/RestDocsMvcTest.kt
+++ src/test/kotlin/com/debate/pangyeori/support/RestDocsMvcTest.kt
@@ -1,6 +1,8 @@
 package com.debate.pangyeori.support
 
 import com.debate.pangyeori.support.containers.TestContainersInitializer
+import jakarta.persistence.EntityManager
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -22,7 +24,11 @@ import org.springframework.web.context.WebApplicationContext
  *
  * 상속하면 전체 Spring 컨텍스트 기반의 MockMvc가 자동으로 준비되고,
  * 각 테스트가 끝날 때 DB 변경이 자동으로 롤백된다.
- * MySQL/Redis(Valkey)는 TestContainers 싱글턴으로 기동되어 테스트 전체에서 공유된다.
+ * 테스트에 필요한 인프라 컨테이너는 TestContainers 싱글턴으로 기동되어 테스트 전체에서 공유된다.
+ *
+ * 각 테스트가 끝나면 롤백 직전에 영속성 컨텍스트를 강제로 flush 한다.
+ * 롤백만으로는 커밋 시점 flush에서만 드러나는 영속성 예외를 놓칠 수 있어,
+ * `@AfterEach`에서 flush를 강제해 실제 커밋에 가까운 검증을 하도록 한다.
  */
 @SpringBootTest
 @ContextConfiguration(initializers = [TestContainersInitializer::class])
@@ -32,6 +38,9 @@ abstract class RestDocsMvcTest {
 
     @Autowired
     private lateinit var context: WebApplicationContext
+
+    @Autowired
+    private lateinit var entityManager: EntityManager
 
     protected lateinit var mockMvc: MockMvc
 
@@ -49,5 +58,10 @@ abstract class RestDocsMvcTest {
                     .withResponseDefaults(Preprocessors.prettyPrint())
             )
             .build()
+    }
+
+    @AfterEach
+    fun flushPersistenceContext() {
+        entityManager.flush()
     }
 }
